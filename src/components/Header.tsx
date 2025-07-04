298b053d4acb1641562e4f924c2e83e9cd8e2f4c
Shiphira  Wamaitha <shiphirawamaitha@gmail.com>
2025-07-04 12:23:20 +0300

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,14 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
-  const cardClasses = darkMode ? 'bg-gray-700 text-white' : 'bg-white text-gray-900';
+  const card = darkMode ? 'bg-gray-700 text-white' : 'bg-white text-gray-900';
 
   return (
-    <header className={`${cardClasses} shadow-sm`}>
+    <header className={`${card} shadow-sm`}>
       <div className="container mx-auto px-4 py-6 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Where in the world?</h1>
+        <h1 className="text-2xl font-bold text-red-400">Where in the world?</h1>
         <button onClick={toggleDarkMode} className="flex items-center space-x-2 hover:opacity-75">
-          {darkMode ? <Sun size={20} /> : <Moon size={20} />}
+          {darkMode ? <Sun size={30} /> : <Moon size={30} />}
           <span>Dark Mode</span>
         </button>
       </div>
